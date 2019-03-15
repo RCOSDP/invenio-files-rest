f64ad9dbdfe84fecccd1e06a0abcc650e544e32a
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-15 18:50:16 +0700

    [sp5][register user name & public flag] - revert to weko/feature.

--- invenio_files_rest/serializer.py
+++ invenio_files_rest/serializer.py
@@ -32,7 +32,7 @@ from marshmallow import Schema, fields, missing, post_dump
 
 from .errors import FilesException
 from .models import Bucket, MultipartObject, ObjectVersion, Part
-from .proxies import current_files_rest
+
 
 class BaseSchema(Schema):
     """Base schema for all serializations."""
@@ -78,12 +78,7 @@ class ObjectVersionSchema(BaseSchema):
     delete_marker = fields.Boolean(attribute='deleted')
     created_user_id = fields.Integer()
     updated_user_id = fields.Integer()
-    uploaded_owners = fields.Method('upload_owners', dump_only=True)
-    
-    def upload_owners(self, o):
-        """ get upload owner information. """
-        return current_files_rest.upload_file_owner_factory(self.created_user_id, self.updated_user_id)
-        
+
     def dump_links(self, o):
         """Dump links."""
         params = {'versionId': o.version_id}
@@ -335,19 +330,3 @@ def json_serializer(data=None, code=200, headers=None, context=None,
         response.set_etag(etag)
 
     return response
-
-def file_uploaded_owner(created_user_id = 0, updated_user_id = 0):
-    """Build upload file owners.
-
-    :param created_user_id: The created user id. (Default: ``0``)
-    :param updated_user_id: The updated user id. (Default: ``0``)
-    :returns: A response with json data.
-    """
-    return {
-        'created_user_id' : created_user_id,
-        'created_username' : '',
-        'created_displayname' : '',
-        'updated_user_id' : updated_user_id,
-        'updated_username' : '',
-        'updated_displayname' : '',
-    }
